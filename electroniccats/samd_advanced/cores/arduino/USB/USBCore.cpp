71fbf069c5c5eae343f39bc4e1d033a1c2c8a3d2
sabas1080 <s@electroniccats.com>
2020-04-08 09:05:46 -0500

    Set USB SRAM QoS to sensitive latency

--- electroniccats/samd_advanced/cores/arduino/USB/USBCore.cpp
+++ electroniccats/samd_advanced/cores/arduino/USB/USBCore.cpp
@@ -387,6 +387,8 @@ void USBDeviceClass::init()
 	usbd.reset();
 
 	usbd.calibrate();
+	usbd.setDataSensitiveQoS();
+	usbd.setConfigSensitiveQoS();
 	usbd.setUSBDeviceMode();
 	usbd.runInStandby();
 	usbd.setFullSpeed();
